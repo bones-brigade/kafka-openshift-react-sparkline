481e1427738b5eab8175eea62a216f1c0c8364c3
michael mccune <msm@opbstudios.com>
2018-08-12 03:28:55 -0400

    add basic sparkline chart

--- src/index.js
+++ src/index.js
@@ -3,8 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'patternfly/dist/css/patternfly.css';
 import 'patternfly/dist/css/patternfly-additions.css';
-import 'jquery/src/jquery';
-import 'bootstrap/dist/js/bootstrap.js';
+import $ from 'jquery/src/jquery';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import c3 from 'c3/c3.min.js';
+import 'd3/d3.min.js';
+import 'patternfly/dist/js/patternfly.js';
+
+class Sparkline extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        columns: [
+          ['%', 10, 14, 12, 20, 31, 27, 44, 36, 52, 55, 62, 68, 69, 88, 74, 88, 91],
+        ],
+        type: 'area'
+      }
+    };
+  }
+
+  componentDidMount() {
+    var c3ChartDefaults = $().c3ChartDefaults();
+    var sparklineChartConfig = c3ChartDefaults.getDefaultSparklineConfig();
+    sparklineChartConfig.bindto = '#sparkline-chart';
+    sparklineChartConfig.data = this.state.data;
+    c3.generate(sparklineChartConfig);
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="sparkline-chart" class="chart-pf-sparkline"></div>
+      </div>
+    );
+  }
+}
 
 class Console extends React.Component {
   render() {
@@ -15,6 +48,11 @@ class Console extends React.Component {
             <img src="/brand.svg" alt="Kafka Charter"/>
           </div>
         </nav>
+        <div class="row">
+          <div class="col-xs-8 col-xs-offset-2">
+            <Sparkline />
+          </div>
+        </div>
       </div>
     );
   }
